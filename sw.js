134c6a60be9c2620dd05ad216b6524f032c48d2a
tiktiny <yuval@tiktin.com>
2026-09-22 23:10:57 +0300

    Refresh cache after restoring V3 interface

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* טיקטין — גנרטורים וחיבורים · שירות אופליין */
-var CACHE = "tiktin-gen-v7";
+var CACHE = "tiktin-gen-v8";
 var ASSETS = ["./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-192-maskable.png",
   "./icon-512-maskable.png", "./apple-touch-icon.png",
