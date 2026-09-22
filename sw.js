1e5eb0eaab03adfb860d1bf4512f2a164b5b3796
tiktiny <yuval@tiktin.com>
2026-09-22 22:38:39 +0300

    Deploy V3 application update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* טיקטין — גנרטורים וחיבורים · שירות אופליין */
-var CACHE = "tiktin-gen-v4";
+var CACHE = "tiktin-gen-v5";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-192-maskable.png",
   "./icon-512-maskable.png", "./apple-touch-icon.png",
